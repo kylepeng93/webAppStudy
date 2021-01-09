952be6619da3f22b84f3fc88800d1663ddb9f8b0
pengkai <kylepeng93@gmail.com>
2021-01-09 16:46:29 +0800

    添加了全局的异常处理器

---
+++ src/main/java/com/linux4fun/controller/GlobalControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.linux4fun.controller;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局的异常处理控制器
+ */
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public void handleConflict() {
+
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) throws Exception {
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+}
